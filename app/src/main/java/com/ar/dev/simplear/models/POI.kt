4441a2a8c9f375302748a9360a29ae1fbd42da46
Devayan Sarkar <devayansarkar@gmail.com>
2017-10-28 12:38:42 +0200

    adding models

---
+++ app/src/main/java/com/ar/dev/simplear/models/POI.kt
@@ -0,0 +1,10 @@
+package com.ar.dev.simplear.models
+
+
+class POI {
+    private var mId: Int = 0
+    private var mName: String? = null
+    private var mDescription: String? = null
+    private var mLatitude: Double = 0.toDouble()
+    private var mLongitude: Double = 0.toDouble()
+}
